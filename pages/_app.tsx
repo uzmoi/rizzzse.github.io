ccd75bc0ceed90111ea236f9587b72b0b445b0e2
rizzzse <rizzzse@gmail.com>
2022-01-01 22:52:23 +0900

    add chakra-ui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,12 @@
+import { ChakraProvider } from "@chakra-ui/react";
 import { AppProps } from "next/app";
 
 const App = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />;
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 };
 
 export default App;
---
+++ pages/_document.tsx
@@ -0,0 +1,17 @@
+import { ColorModeScript } from "@chakra-ui/react";
+import NextDocument, { Head, Html, Main, NextScript } from "next/document";
+
+export default class Document extends NextDocument {
+  render(): JSX.Element {
+    return (
+      <Html>
+        <Head />
+        <body>
+          <ColorModeScript initialColorMode="system" />
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
